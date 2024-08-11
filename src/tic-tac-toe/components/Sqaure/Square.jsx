6ad3995127a62ee99016c563bc8542675f46c0fc
soyeonpaark <thdus906@naver.com>
2024-08-11 23:16:14 +0900

    속성에 따른 화면 렌더링 구상, 속성 검사, CSS 모듈 설정

--- src/tic-tac-toe/components/Sqaure/Square.jsx
+++ src/tic-tac-toe/components/Sqaure/Square.jsx
@@ -1,7 +1,28 @@
-function Square() {
+// --------------------------------------------------------------------------
+// ✅ 틱택토 게임 만들기 (Square 컴포넌트)
+// --------------------------------------------------------------------------
+// - [x] 아토믹 컴포넌트인 Square 컴포넌트를 화면에 표시되도록 구현할 것입니다.
+// - [x] Square 컴포넌트가 속성(props)에 따라 화면에 어떻게 렌더링되어야 하는 지 설계합니다.
+//       - [상황 0] 아무런 콘텐츠를 화면에 표시하지 않는다.
+//       - [상황 1] 사용자 액션이 있을 경우, 현재 플레이어를 화면에 표시한다.
+//       - [상황 2] 이미 사용자 액션이 처리된 경우, 화면에 아무런 변화가 없다.
+// - [x] Square 컴포넌트 속성(props) 검사를 작성합니다. (TypeScript or prop-types)
+// - [x] Square 컴포넌트의 스타일을 CSS 모듈(modules) 방식으로 설정합니다. (with Vite)
+// --------------------------------------------------------------------------
+
+import { node } from 'prop-types';
+import S from './Square.module.css';
+
+Square.propTypes = {
+  children: node,
+};
+
+function Square({ children }) {
+  const isDisabled = !!children;
+
   return (
-    <button className="Square" disabled>
-      🟨
+    <button className={S.component} disabled={isDisabled}>
+      {children}
     </button>
   );
 }
--- src/tic-tac-toe/components/Sqaures/Squares.jsx
+++ src/tic-tac-toe/components/Sqaures/Squares.jsx
@@ -1,17 +1,12 @@
+import { PLAYER } from '@/tic-tac-toe/constants';
 import Square from '../Sqaure/Square';
 
 function Squares() {
   return (
     <div className="Squares">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square></Square>
+      <Square>{PLAYER.ONE}</Square>
+      <Square>{PLAYER.TWO}</Square>
     </div>
   );
 }
